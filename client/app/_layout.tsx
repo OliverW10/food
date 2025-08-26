04c3a677a40b79eb98e393b6adf1c06cbe95e50b
Oliver Warrick <oliver.warrick2@gmail.com>
2025-08-26 14:00:24 +1000

    add gluestack

--- client/app/_layout.tsx
+++ client/app/_layout.tsx
@@ -1,5 +1,21 @@
 import { Stack } from "expo-router";
 
+import { Fab, FabIcon } from "@/components/ui/fab";
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
+import { AddIcon } from "@/components/ui/icon";
+import "@/global.css";
+
 export default function RootLayout() {
-  return <Stack />;
+  return <GluestackUIProvider mode="light">
+      <Fab
+        size="lg"
+        placement="bottom right"
+        isHovered={false}
+        isDisabled={false}
+        isPressed={false}
+      >
+        <FabIcon as={AddIcon} />
+      </Fab>
+    <Stack />
+  </GluestackUIProvider>;
 }
