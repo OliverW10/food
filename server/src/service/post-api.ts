8ad84cf623590bd1b8a8317b9dda6343cb420267
Olivia Stewart <olivia.stewart@wisetechglobal.com>
2025-08-29 09:44:11 +1000

    post api

---
+++ server/src/service/post-api.ts
@@ -0,0 +1,46 @@
+import { TRPCError } from "@trpc/server";
+import { publicProcedure, router } from "../trpc";
+import {
+    createPostInputSchema,
+    idInputSchema,
+    postOutputSchema,
+    postsOutputSchema,
+} from "./post-schemas";
+import {
+    createPost,
+    deletePost,
+    getAllPosts,
+    getPostById,
+} from "./post-service";
+
+export const postApi = router({
+  create: publicProcedure
+    .input(createPostInputSchema)
+    .output(postOutputSchema)
+    .mutation(async ({ input }) => {
+      const post = await createPost(input);
+      return post;
+    }),
+  getById: publicProcedure
+    .input(idInputSchema)
+    .output(postOutputSchema.optional())
+    .query(async ({ input }) => {
+      const post = await getPostById(input.id);
+      return post ?? undefined;
+    }),
+  getAll: publicProcedure.output(postsOutputSchema).query(async () => {
+    const posts = await getAllPosts();
+    return posts;
+  }),
+  delete: publicProcedure
+    .input(idInputSchema)
+    .output(postOutputSchema)
+    .mutation(async ({ input }) => {
+      const existing = await getPostById(input.id);
+      if (!existing) {
+        throw new TRPCError({ code: "NOT_FOUND", message: "Post not found" });
+      }
+      await deletePost(input.id);
+      return existing;
+    }),
+});
---
+++ server/src/service/post-service.ts
@@ -0,0 +1,34 @@
+import { db } from "../db";
+import { CreatePostInput } from "./post-schemas";
+
+export async function createPost(post: CreatePostInput) {
+    return db.post.create({
+        data: {
+            title: post.title,
+            ...(post.foodId && { food: { connect: { id: post.foodId } } }),
+            author: { connect: { id: post.authorId } },
+        },
+    });
+}
+
+export function getPostById(postId: number) {
+    return db.post.findUnique({
+        where: { id: postId },
+    });
+}
+
+export function getPostsByAuthor(authorId: number) {
+    return db.post.findMany({
+        where: { authorId },
+    });
+}
+
+export function getAllPosts() {
+    return db.post.findMany();
+}
+
+export function deletePost(postId: number) {
+    return db.post.delete({
+        where: { id: postId },
+    });
+}
