bcccdcb23914277ed4aab68427d0fb4daae8d7d9
Rianna <82356846+rixnnalxxdy@users.noreply.github.com>
2025-09-19 15:25:33 +1000

    fixed test deleted unneeded mocks

--- client/services/trpc.ts
+++ client/services/trpc.ts
@@ -2,7 +2,7 @@ import { createTRPCReact } from '@trpc/react-query';
 import Constants from 'expo-constants';
 import type { AppRouter } from '../../server/src/main';
 
-const configuredServerUrl = Constants.expoConfig?.extra!.apiUrl;
+const configuredServerUrl = Constants.expoConfig?.extra?.apiUrl ?? process.env.API_URL ?? 'http://localhost:3000';
 const expoServerIp = Constants.expoConfig?.hostUri?.split(":")[0]
 const expoServerUrl = expoServerIp !== undefined ? `http://${expoServerIp}:3000` : undefined;
 const localhostServerUrl = `http://localhost:3000`;
